1a12f43242f8ad6f93529f1abb43612226ed56f3
LukeJakielaszek <tug52339@temple.edu>
2020-04-17 10:05:46 -0700

    Wrote documentation and added comments

--- README.md
+++ README.md
@@ -1,9 +1,31 @@
-
-Basic starter code for Raft leader election project
-
-raftnode.py should implement the code for a single RAFT node.
-
-You will likely want to implement some type of client that can
-query your RAFT nodes to determine which one of them, if any,
-is a leader.
 # raft_distributed_consensus
+CIS 4307 - Final Project - Luke Jakielaszek - 915377673 - tug52339
+
+## Application Overview
+The application inherits from the rpyc service. Therefore, the requestvote and appendentries are implemented as exposed methods. All rpc calls come through the rpyc threadpoolserver instance. The leader election and heartbeat logic is implemented through a secondary thread which calls run_asynch/run_server methods. To ensure all information remains consistent across threads, I implemented a mutex which locks the object anytime we update state/term info. However, to prevent deadlock, during heartbeats and multicasting requestvotes, I stored the servers state info in a temporary variable, performed the multicasting, then re-acquired the lock for the server and checked if our state had changed while multicasting (another server calls an rpc on us). If our state had changed, I throw out the results from the multicast/heartbeat, otherwise I securely update the state/term/election info for the object and continue as normal.
+
+### RequestVoteRPC
+The requestvote rpc models takes in the candidates term and id. Before doing anything, it acuqires the nodes lock, ensuring that updates are synchronized. It rejects the candidate if the current term is higher than the candidates or the node has already voted. It accepts the candidate if the candidates term is larger than its current term or it has not yet voted during the current election.
+
+### AppendEntries RPC
+The appendentries rpc models takes in the leaders term and id. Before doing anything, it acuqires the nodes lock, ensuring that updates are synchronized. It rejects the appendentries if the current term is higher than the leaders. It accepts the appendentries in all other cases and resets its timer.
+
+### Heartbeat
+If a server is the leader, it will send out a heartbeat. At the start of the heartbeat, the server object is locked and the state / term info is stored in temporary variables. Then the server is unlocked. This avoids deadlock in the distributed system when calling appendentries on other nodes in the network. The appendentries is then multicasted to all other nodes in the network. If false is returned, the leaders temporary state is set to follower and the term is updated. After multicasting, the leader will acquire its own lock and check if any rpcs were called on it during multicasting that affected its state. If not, it securely updates its state information that was gathered during the heartbeat. If so, it throws out the results from the heartbeat.
+
+### Multicast
+If a server is a candidate, it will send out a multicast requestVote rpc. At the start of this multicast, the server object is locked and the state / term info is stored in temporary variables. Then the server is unlocked. This avoids deadlock in the distributed system when calling requestVote rpcs on other nodes in the network. The requestVote is then multicasted to all other nodes in the network. If false is returned, the current node checks if the nodes term is higher than its temporary term. If so, it converts its temporary states to follower. otherwise, it ignores the result as the node has already voted for someone. If True is returned, it increments its temporary count of voted nodes and checks to see if it has acquired more than half of the votes for the current election (and therefore become leader). After multicasting, the candidate will acquire its own lock and check if any rpcs were called on it during multicasting that affected its state. If not, it securely updates its state information that was gathered during the heartbeat. If so, it throws out the results from the multicast requestVote rpc.
+
+## Limitations
+As we have only implemented the leader election portion of the protocol, there is no log replication or meaningful message transfer. In addition, I chose to only save the current term to file rather than the entire state of a server. I chose to do this to limit the occurrence of non-byzantine failures and avoid conflict when updating a file. If a node fails, it will therefore start again as a follower from its current term, then timeout and start an election or be picked up by another leader. By not storing state, the raft protocol will just start a new election on timeout and proceed as normal. However, if I were to store state in addition to term, the probability of having a failure while updating a state file would increase and possibly lead to multiple nodes being leader during a single term, which should be guarenteed to not happen in raft.
+
+For example, if our file stores data in format term,state such as below:
+1,leader
+
+If we went to update both the term and state after receiving an RequestVote RPC from another node to start a new election, we would expect the following:
+2,candidate
+
+However, if we killed a node partway through this update, we may only have been able to write the term, but not update the state and get the following:
+2,leader
+
+Therefore, on restart the node would declare itself the leader of a term in which it never won an election, which is bad. To avoid this, I only stored term and defaulted to follower on restart.
--- raftnode.py
+++ raftnode.py
@@ -8,11 +8,13 @@ import os
 import threading
 import logging
 
+# set up a custom logger
 def record_factory(*args, **kwargs):
     record = old_factory(*args, **kwargs)
     record.custom_attribute = "Node " + sys.argv[2]
     return record
 
+# more logger configuration
 logging.basicConfig(filename="logfile_" + sys.argv[2] + '.log', format="%(asctime)s - %(custom_attribute)s - %(message)s", filemode='w')
 old_factory = logging.getLogRecordFactory()
 logging.setLogRecordFactory(record_factory)
@@ -21,6 +23,7 @@ logger=logging.getLogger()
 #Setting the threshold of logger to DEBUG 
 logger.setLevel(logging.DEBUG) 
 
+# runs the actual raft algorithm for the class
 def run_asynch(temp):
     temp.run_server()
 
@@ -33,23 +36,54 @@ other methods to implement ONLY the leader election part of the RAFT
 protocol.
 '''
 class RaftNode(rpyc.Service):
+    # holds meta info about all nodes in configuration
     other_nodes_meta = {}
+
+    # number of nodes in configuration
     total_nodes = None
+
+    # number of nodes to get majority
     threshold = None
+
+    # the current nodes id
     node_id = None
+
+    # port of our node
     port = None
-    term = None
+
+    # ip of our node
     hostname = None
+
+    # the current term
+    term = None
+
+    # mutual exclusion between this thread and rpc calls from other servers
     state_lock = None
+
+    # this nodes state
     state = None
+
+    # length until random timeout
     timeout = None
+
+    # most recent reset
     clock = None
+
+    # constants to define state
     FOLLOWER_STATE = 0
     CANDIDATE_STATE = 1
     LEADER_STATE = 2
+
+    # boolean to say if we are the leader
     active_leader = None
+
+    # disk location where we store our term
     term_file = None
+
+    # if we have already voted
     voted_for = None
+
+    # if we are a participant in an election
     active_election = False
     
     '''
@@ -64,7 +98,7 @@ class RaftNode(rpyc.Service):
         # find term file
         self.term_file = "./tmp/" + str(node_id) + "_term.txt"
 
-        #get most up to date term or defaul to zero
+        #get most up to date term or default to zero
         if os.path.exists(self.term_file):
             logging.debug('getting term from file')
             with open(self.term_file, "r") as tf:
@@ -135,6 +169,7 @@ class RaftNode(rpyc.Service):
         time_passed = now - self.cur_clock
 
         if(time_passed > self.timeout):
+            # reset our timer if timeout detected
             self.reset_timer()
             
             return True
@@ -144,7 +179,8 @@ class RaftNode(rpyc.Service):
     # resets the internal clock
     def reset_timer(self):
         self.cur_clock = time.time()
-        
+
+    # returns if we are the current leader
     def isLeader(self):
         return(self.state == self.LEADER_STATE)
     
@@ -153,8 +189,10 @@ class RaftNode(rpyc.Service):
         logging.debug("Running server")
         # run indefinitely
         while True:
+            # lock our nodes state
             self.state_lock.acquire()
-            
+
+            # check if we are leader
             if(self.isLeader()):
                 # if leader, send a heartbeat
                 self.heartbeat()
@@ -163,28 +201,34 @@ class RaftNode(rpyc.Service):
                 if(self.is_timeout()):
                     # the timer timedout
                     logging.debug("Timout Detected")
+
+                    # transition us to a candidate state for a new election
                     self.become_candidate()
                 elif self.state == self.CANDIDATE_STATE:
+                    # timer did not time out
                     # if candidate in active election multicast to others
                     self.multicast_request()
                     
-            # attempt to release the lock
+            # attempt to release the lock if we have not already released it
             try:
                 self.state_lock.release()
             except:
                 pass
 
+            # slow it down so we can see whats going on in logfiles
             time.sleep(.5)
             
-    # multicast rpc
+    # multicast requestVote rpc to all nodes
     def multicast_request(self):
         logging.debug('Starting request_vote multicast')
-        # store current state information
+        # store current state information in temporary variables
         new_term = self.term
         old_term = new_term
         new_state = self.state
         active_election = True
         vote_count = self.vote_count
+
+        # release the lock to avoid deadlock
         self.state_lock.release()
 
         # assume nothing is wrong
@@ -212,8 +256,10 @@ class RaftNode(rpyc.Service):
                         invalid = True
                         break
                 else:
-                    # node votes for us
+                    # rpc successful, node votes for us
                     vote_count += 1
+
+                    # check if we won election
                     if(vote_count >= self.threshold):
                         # I am leader.
                         new_state = self.LEADER_STATE
@@ -221,9 +267,10 @@ class RaftNode(rpyc.Service):
                         break
             except:
                 print("Could not connect to [" + addr + ':' + str(port) + "]")
-                
+
+        # re-synchronize our state
         with self.state_lock:
-            # if another node did not call an rpc on us with multicasting a heartbeat
+            # if another node did not call an rpc on us with multicasting a requestvote
             if self.term == old_term:
                 logging.debug('No RPC called on us during requestVote multicast')
                 self.print_self()                
@@ -238,7 +285,7 @@ class RaftNode(rpyc.Service):
                     self.print_self()
                     self.active_election = False
                     self.print_self()
-                # if i am leader
+                # if i am leader (won election)
                 elif(not active_election):
                     # declare self as leader
                     logging.debug('\trequestVote RPC success. I won election. I am leader')
@@ -252,21 +299,24 @@ class RaftNode(rpyc.Service):
                     self.active_election = False
                     self.print_self()
                 else:
+                    # node voted for us, but we didnt win yet
                     logging.debug('\trequestVote RPC success. Continuing election')
                     if(self.vote_count == vote_count):
                         logging.debug('\tNode already voted')
                     self.vote_count = vote_count
                     self.print_self()
             else:
+                # cancel results from current multicast as our state is no longer synchonized
                 logging.debug('RPC called on us during requestVote multicast. Cancelling result')
                 self.print_self()
 
-                
+    # display interesting node information
     def print_self(self):
         logging.debug("\t\tNode : " + str(self.node_id) + "- Term : "
-                      + str(self.term) + " - State : " + str(self.state) + " - VCount : " + str(self.vote_count))
+                      + str(self.term) + " - State : " + str(self.state)
+                      + " - VCount : " + str(self.vote_count))
         
-    # reset the votes
+    # reset the votes for ourself
     def reset_votes(self):
         self.voted_for = None
         self.vote_count = 0
@@ -294,6 +344,7 @@ class RaftNode(rpyc.Service):
     def exposed_is_leader(self):
         return(self.isLeader())
 
+    # requests node for their vote in election
     def exposed_request_vote(self, can_term, can_id):
         logging.debug('RequestVote RPC ' + str(can_term) + " " + str(can_id))
         # acquire the state lock
@@ -306,7 +357,7 @@ class RaftNode(rpyc.Service):
                 return(False, self.term)
             # if can term is larger than our own
             elif(can_term > self.term):
-                # convert to follower of candidate
+                # convert to follower of candidate, vote for them
                 logging.debug('\tAccept: Candidate term larger - Voting for ' + str(can_id))
 
                 self.update_term(can_term)
@@ -317,13 +368,14 @@ class RaftNode(rpyc.Service):
             else:
                 # candidate is part of the current election
                 if self.voted_for is None:
-                    # we have not yet voted for anyone or voted for the candidate already
+                    # we have not yet voted for anyone, vote for candidate
                     logging.debug('\tAccept: Participant Voting for ' + str(can_id))
                     self.voted_for = can_id
                     
                     # vote for candidate
                     return(True, self.term)
                 else:
+                    # we have already voted during the current election
                     logging.debug('\tReject: I have already voted ' + str(can_id))
                     # we have already voted
                     return(False, self.term)
@@ -335,6 +387,8 @@ class RaftNode(rpyc.Service):
         new_term = self.term
         old_term = new_term
         new_state = self.state
+
+        # release the lock to avoid deadlock
         self.state_lock.release()
 
         # assume nothing is wrong
@@ -376,8 +430,10 @@ class RaftNode(rpyc.Service):
                     self.state = new_state
                     self.reset_timer()
                 else:
+                    # heartbeat went fine, continue on as normal
                     logging.debug('Heartbeat success')
             else:
+                # throw everything out since we are no longer in sync
                 logging.debug('RPC called on us during heartbeat. Cancelling result')            
                     
     # append entries rpc
@@ -413,7 +469,9 @@ class RaftNode(rpyc.Service):
                 # notify of valid leader
                 return(True, self.term)
 
+# run the program
 if __name__ == '__main__':
+    # ensure the number of args supplied is correct
     if(len(sys.argv) != 4):
         print("ERROR: Invalid number of params - Required : 4 - Given : " +
               str(len(sys.argv)))
@@ -421,12 +479,15 @@ if __name__ == '__main__':
         
         exit(-1)
 
+    # create an instance of our class
     node = RaftNode(config=sys.argv[1], node_id=int(sys.argv[2]), port=int(sys.argv[3]))
     print('Attempting to run server')
 
+    # start a thread to run the synchronization of our server
     t = threading.Thread(target=run_asynch, args=(node,))
     t.start()
-    
+
+    # start the threadpoolserver which waits to accept incoming rpc
     logging.debug('Server running')
     server = ThreadPoolServer(node, port=int(sys.argv[3]))
     server.start()
